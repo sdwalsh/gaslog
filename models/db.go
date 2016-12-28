b9c787978e66c1d48ebf5a2ebc016adb28579233
Sean Walsh <sdwalsh@mirango.io>
2016-12-28 15:35:06 -0500

    create database interface

---
+++ models/db.go
@@ -0,0 +1,47 @@
+package models
+
+import (
+	"io/ioutil"
+	"math/big"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type Datastore interface {
+	InsertUser(string, []byte, string) (*User, error)
+	GetUserByEmail(string) (*User, error)
+	GetUserById(string) (*User, error)
+
+	GetLogsByUser(string) (*[]Logs, error)
+	InsertLog(string) (*Logs, error)
+	UpdateLog(string) (*Logs, error)
+	DeleteLog(string) error
+
+	GetEntriesByUser(string) (*[]Entry, error)
+	GetEntriedByLog(string) (*[]Entry, error)
+	InsertEntry(string, *big.Int, float64, float64, string) (*Entry, error)
+	DeleteEntry(string) error
+}
+
+type Data struct {
+	*sqlx.DB
+}
+
+func CreateDB(database string, connectionOptions string) (*Data, error) {
+	// Import database file and convert to string
+	schema, err := ioutil.ReadFile(database)
+	if err != nil {
+		return nil, err
+	}
+	sql := string(schema[:])
+
+	db, err := sqlx.Connect("postgres", connectionOptions)
+	if err != nil {
+		return nil, err
+	}
+
+	db.MustExec(sql)
+
+	return &Data{db}, nil
+}
